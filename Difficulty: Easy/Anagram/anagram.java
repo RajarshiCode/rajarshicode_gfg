e3f78eb578e7d858c141c59a4c99cc02b43db250
Rajarshi Das <rajarshidas47@gmail.com>
2024-08-05 00:10:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,48 +1,39 @@
 //{ Driver Code Starts
-import java.util.*;
-import java.util.stream.*; 
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
 
 class GFG {
-    
-	public static void main (String[] args)throws IOException {
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		while(t-->0)
-		{
-		    String str = br.readLine();
-		    String s1 = str.split(" ")[0];
-		    String s2 = str.split(" ")[1];
-		    
-		    Solution obj = new Solution();
-		    
-		    if(obj.isAnagram(s1,s2))
-		    {
-		        System.out.println("YES");
-		    }
-		    else
-		    {
-		         System.out.println("NO");
-		    }
-		    
-		    
-		    
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str = br.readLine();
+            String s1 = str.split(" ")[0];
+            String s2 = str.split(" ")[1];
+
+            Solution obj = new Solution();
+
+            if (obj.isAnagram(s1, s2)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
 // } Driver Code Ends
 
 
-class Solution
-{    
-    //Function is to check whether two strings are anagram of each other or not.
-    public static boolean isAnagram(String a,String b)
-    {
-        
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a, String b) {
+
         // Your code here
-        String str1 = a.replaceAll("\\s","");
+         String str1 = a.replaceAll("\\s","");
         String str2 = b.replaceAll("\\s","");
         
         if(str1.length()!=str2.length())
@@ -54,7 +45,5 @@ class Solution
             Arrays.sort(ch2);
             return Arrays.equals(ch1,ch2);
         }    
-           
-        
     }
 }
